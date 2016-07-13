a462d3c629901e427bc25cba7d3b78d056681036
Katherine Crowson <crowsonkb@gmail.com>
2016-07-12 23:17:48 -0700

    Fix export of save_as_exr()
    Former-commit-id: 76b2b7ad551d6e2a1b160b89438dd16526416e24

--- deep_dream/__init__.py
+++ deep_dream/__init__.py
@@ -1,3 +1,3 @@
-from .deep_dream import CNN, EPS, ShapeError, to_image
+from .deep_dream import CNN, EPS, ShapeError, save_as_exr, to_image
 from .deep_dream import CNNData, GOOGLENET_BVLC, GOOGLENET_PLACES205, GOOGLENET_PLACES365
 del deep_dream
