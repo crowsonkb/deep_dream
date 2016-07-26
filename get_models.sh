ae7ae1a5e4f35c94ec080f2291615a021d420d62
Katherine Crowson <crowsonkb@gmail.com>
2016-07-26 11:37:09 -0700

    Move googlenet_places205 .caffemodel onto S3

--- get_models.sh
+++ get_models.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 BVLC='http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel'
-PLACES205='http://places.csail.mit.edu/model/googlenet_places205.tar.gz'
+PLACES205='https://s3-us-west-2.amazonaws.com/crowsonkb-deep-dream/googlelet_places205_train_iter_2400000.caffemodel'
 PLACES365='http://places2.csail.mit.edu/models_places365/googlenet_places365.caffemodel'
 RESNET50='http://www.deepdetect.com/models/resnet/ResNet-50-model.caffemodel'
 
 ( cd bvlc_googlenet && curl -O "$BVLC" )
-( cd googlenet_places205 &&
-  curl "$PLACES205" | tar xzv &&
-  cp googlenet_places205/*.caffemodel . &&
-  rm -rf googlenet_places205 )
+( cd googlenet_places205 && curl -O "$PLACES205" )
 ( cd googlenet_places365 && curl -O "$PLACES365" )
 ( cd resnet && curl -O "$RESNET50" )
